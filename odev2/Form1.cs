21a31cc338377585c74da3d094d21f652286db90
Mohamad alchekh <m.alchekh@outlook.com>
2023-10-26 14:38:42 +0300

    odev 2

---
+++ odev2/Form1.cs
@@ -0,0 +1,49 @@
+﻿namespace odev2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add("İşçi");
+            comboBox1.Items.Add("muhendis");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Personel pers = new Personel
+         (AdText.Text, AdresText.Text, comboBox1.Text, Convert.ToInt32(YasText.Text), Convert.ToInt32(MesaiText.Text));
+
+            if (comboBox1.Text == "İşçi")
+
+            {
+                MessageBox.Show(pers.ucretHesapla().ToString());
+
+            }
+            else
+            {
+                pers.ucretHesapla(1000);
+            }
+        }
+    }
+}
